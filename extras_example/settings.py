269691e7dc238c1544da976cf071445d207b8af6
Tim Savage <tim.savage@poweredbypenguins.org>
2013-10-23 00:43:12 +1100

    Added custom_fields django "app"

--- extras_example/settings.py
+++ extras_example/settings.py
@@ -123,6 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'django.contrib.admin',
+
+    'extras_example.custom_fields',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
