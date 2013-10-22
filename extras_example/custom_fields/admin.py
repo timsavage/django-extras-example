587d2f2c63ef401b32418d0aefe052438a332f8a
Tim Savage <tim.savage@poweredbypenguins.org>
2013-10-23 01:02:11 +1100

    Add example field for monitary values

--- extras_example/custom_fields/admin.py
+++ extras_example/custom_fields/admin.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.contrib.admin import site
+from extras_example.custom_fields.models import Example
 
+
+site.register(Example)
--- extras_example/custom_fields/models.py
+++ extras_example/custom_fields/models.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
+from django_extras.db import models
+
+
+class Example(models.Model):
+    price = models.MoneyField()
 
